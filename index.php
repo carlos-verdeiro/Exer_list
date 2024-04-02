4b2f1c7e554b874a62b2806453e58890a93a7030
Carlos Daniel Verdeiro <carlos.d.verdeiro@gmail.com>
2024-04-02 14:03:07 -0300

    criação inicial

--- index.php
+++ index.php
@@ -4,12 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/custom.css">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Menu principal</title>
 </head>
+<body class="bg-dark-subtle ">
+    <main>
+        <section class="lista">
+            <a href="pages/lerNum.php" class="btn btn-primary">Ler 5 números</a>
+            <a href="pages/pop.php" class="btn btn-primary">População país</a>
+            <a href="pages/pII.php" class="btn btn-primary">Pares e Ímpares com intervalo</a>
+            <a href="pages/tab.php" class="btn btn-primary">Tabuada</a>
+            <a href="pages/eleva.php" class="btn btn-primary">Elevação</a>
+            <a href="pages/pI.php" class="btn btn-primary">Pares e Ímpares</a>
 
-<body>
-    
+        </section>
+    </main>
 </body>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </html>
