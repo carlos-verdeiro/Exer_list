0b74d8d6ef4eea49120280ec94e3c95ceb9cee66
Carlos Daniel Verdeiro <carlos.d.verdeiro@gmail.com>
2024-04-02 15:17:31 -0300

    continuação

--- pages/lerNum.php
+++ pages/lerNum.php
@@ -1,11 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../assets/css/atv.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Ler Num</title>
 </head>
-<body>
-    
-</body>
+<body class="bg-dark-subtle ">
+    <main>
+        
+        <form method="get">
+            <input type="number" name="n1" id="n1" required>
+            <input type="number" name="n2" id="n2" required>
+            <input type="number" name="n3" id="n3" required>
+            <input type="number" name="n4" id="n4" required>
+            <input type="number" name="n5" id="n5" required>
+
+            <input type="submit" value="Calcular">
+        </form>
+        
+    </main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </html>
