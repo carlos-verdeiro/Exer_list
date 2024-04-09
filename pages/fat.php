4e371aa4261b4aa10d6381bcea5f8dd49cc596fd
Carlos Daniel Verdeiro <carlos.d.verdeiro@gmail.com>
2024-04-09 13:20:31 -0300

    alt titles

--- pages/fat.php
+++ pages/fat.php
@@ -19,7 +19,7 @@ if (isset($_GET['fat'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/atv.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Ler Num</title>
+    <title>Fator</title>
 </head>
 
 <body class="bg-dark-subtle ">
